744f94f356723e07067eefa34bd025f066e7c740
aph-96 <aylahanlon96@gmail.com>
2026-07-19 23:03:47 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.getElementById("nav");
+
+if (menuToggle && nav) {
+  menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+}
